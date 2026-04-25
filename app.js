843056b26209e0ebfb32a378542004b3e19c94e1
Rahul Kumar <rahulkumar.ksrct@gmail.com>
2026-04-25 12:53:10 +0530

    added firebase config file

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-// 🔥 paste your firebase config here
-const firebaseConfig = {
-    apiKey: "AIzaSyCXqXv-YG802yzBwPItL4MLdP1LtfwYdrc",
-    authDomain: "miraj-cinemas-seat-tracker.firebaseapp.com",
-    projectId: "miraj-cinemas-seat-tracker",
-    storageBucket: "miraj-cinemas-seat-tracker.firebasestorage.app",
-    messagingSenderId: "1052153141275",
-    appId: "1:1052153141275:web:5efb211370fb6c8e19c0be",
-    measurementId: "G-Y3R222RVED"
-  };
-
-// init firebase
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
-
 async function start() {
   const permission = await Notification.requestPermission();
 
